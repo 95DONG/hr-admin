50e5f4fe3707044ad233e324992d2dc81b4636bd
DONG <969349879@qq.com>
2022-07-13 17:21:23 +0800

    写登录页面

--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,20 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    // 反向代理
+    proxy: {
+      // 只要是axios发请求的时候，前面是以/abc开头的，就会被这块匹配到，就会自动帮我们做反向代理
+      // 所有的当前的这个人力资源接口url都是以api开头的
+      '/abc': {
+        target: 'http://ihrm.itheima.net/',
+        changeOrigin: true,
+        pathRewrite: {
+          // 这一步相当于replace替换操作
+          // ^这个符号，如果不写。接口中要是在中间的话也会被替换掉
+          '^/abc': ''
+        }
+      }
     }
     // 项目有自己的接口数据，不需要模拟数据
     // before: require('./mock/mock-server.js')
